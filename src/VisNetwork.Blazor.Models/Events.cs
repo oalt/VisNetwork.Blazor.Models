eba82bcdfc63948cae3987b1cb33f4eac0c20fda
Oliver Alt <oa@lacheweg21.de>
2022-12-18 09:57:33 +0100

    Initial code commit of data model classes

---
+++ src/VisNetwork.Blazor.Models/Events.cs
@@ -0,0 +1,83 @@
+using System.Collections.Generic;
+
+namespace VisNetwork.Blazor.Models;
+
+#nullable disable
+public class BaseClickEvent
+{
+    //public Microsoft.JSInterop.DotNetObjectReference<Network> Source { get; set; }
+
+    /// <summary>
+    ///  Selected node ids
+    /// </summary>
+    /// <value></value>
+    public List<string> Nodes { get; set; }
+
+    /// <summary>
+    /// Selected edge ids
+    /// </summary>
+    /// <value></value>
+    public List<string> Edges { get; set; }
+
+    public object Event { get; set; }
+
+    public object Pointer { get; set; }
+}
+
+/// <summary>
+/// Fired when the user clicks the mouse or taps on a touchscreen device.
+/// </summary>
+public class ClickEvent : BaseClickEvent
+{
+    // The order of the items array is descending in z-order. Thus, to get the topmost item, get the value at index 0.
+    public List<ClickedItem> Items { get; set; }
+}
+
+public class ClickedItem
+{
+    public string NodeId { get; set; }
+    public string EdgeId { get; set; }
+    public string LabelId { get; set; }
+}
+
+/// <summary>
+/// Fired when the user double clicks the mouse or double taps on a touchscreen device.
+/// Since a double click is in fact 2 clicks, 2 click events are fired, followed by a double click event.
+/// If you do not want to use the click events if a double click event is fired, just check the time between click events before processing them. 
+/// </summary>
+public class DoubleClickEvent : ClickEvent
+{
+}
+
+public class DeselectClickEvent : BaseClickEvent
+{
+    public NodeEdgeSelection PreviousSelection { get; set; }
+}
+
+
+public class NodeEdgeSelection
+{
+    public IReadOnlyCollection<NodeSelection> Nodes { get; set; }
+    public IReadOnlyCollection<EdgeSelection> Edges { get; set; }
+}
+
+public class NodeSelection
+{
+    public string Id { get; set; }
+    public double X { get; set; }
+    public double Y { get; set; }
+}
+
+public class EdgeSelection
+{
+    public string Id { get; set; }
+    public string FromId { get; set; }
+    public string ToId { get; set; }
+}
+
+public class DrawingEvent
+{
+    public string CanvasDataUrl { get; set; }
+}
+
+#nullable enable
---
+++ src/VisNetwork.Blazor.Models/NodeSelectEventArguments.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VisNetwork.Blazor.Models
+{
+    public class NodeSelectEventArguments : EventArgs
+    {
+        public List<string> SelectedNodes { get; set; }
+    }
+}
